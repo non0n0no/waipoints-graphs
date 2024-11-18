9a5a071b44b8d8d734456998d3ff035df2e42b40
non0n0no <a55120@alunos.ipb.pt>
2024-11-18 20:53:57 +0000

    A simple graph API

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -7,19 +7,13 @@ public class Node
     public List<Edge> edgelist = new List<Edge>();
     public Node path = null;
     GameObject id;
-    public float xPos;
-    public float yPos;
-    public float zPos;
 
     public float f, g, h;
-    public Node CameFrom;
+    public Node cameFrom;
 
     public Node(GameObject i)
     {
         id = i;
-        xPos = i.transform.position.x;
-        yPos = i.transform.position.y;
-        zPos = i.transform.position.z;
         path = null;
     }
 
