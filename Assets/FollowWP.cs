b4a47323f2c600dfcf827272bd1e2516d4616ae8
non0n0no <a55120@alunos.ipb.pt>
2024-11-18 05:25:46 +0000

    following a circuit tracker

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -9,17 +9,28 @@ public class FollowWP : MonoBehaviour
 
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
         {
             currentWP++;
         }
@@ -29,9 +40,16 @@ public class FollowWP : MonoBehaviour
             currentWP = 0;
         }
 
-        //this.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 2) * Time.deltaTime);
+    }
 
-        Quaternion LookAtWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+    // Update is called once per frame
+    void Update()
+    {
+        ProgressTracker();
+
+        Quaternion LookAtWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
 
         this.transform.rotation = Quaternion.Slerp(transform.rotation, LookAtWP, rotSpeed * Time.deltaTime);
 
